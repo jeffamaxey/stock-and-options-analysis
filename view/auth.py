2295357469976d86be1d2936c73120d2bf20eca3
Sahngwoo Kim (sak772) <sak772@mail.usask.ca>
2021-03-09 21:27:43 -0600

    Implemented landing page

--- view/auth.py
+++ view/auth.py
@@ -8,8 +8,22 @@ auth_bp = Blueprint("auth", __name__)
 @auth_bp.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        email = request.form.get("email1")
-        password = request.form.get("password1")
+
+        email = request.form.get("email12")
+        if email is None:
+            email = request.form.get("email19")
+        if email is None:
+            email = request.form.get("email114")
+        if email is None:
+            email = request.form.get("email119")
+
+        password = request.form.get("password11")
+        if password is None:
+            password = request.form.get("password16")
+        if password is None:
+            password = request.form.get("password19")
+        if password is None:
+            password = request.form.get("password112")
 
         if UserDB.has(email):
             if UserDB.check_password(email, password):
@@ -19,7 +33,7 @@ def login():
                 user = UserDB.get(email)
                 login_user(user, remember=True)
 
-                return redirect(url_for("home.home"))  # redirect the user to the homepage
+                return redirect(url_for("general.home"))  # redirect the user to the homepage
             else:
                 flash("Incorrect password, please try again.", category="Error")
         else:
@@ -28,39 +42,100 @@ def login():
     return render_template("login-page.html", user=current_user)
 
 
-MIN_EMAIL_LEN = 4
-MIN_PASSWORD_LEN = 7
+def register(email, password1, password2=None, agreed=None, redirect_url="/", product=None):
+    """
+    Check if the values are valid and if so, add the user to the database
+    :param email: The email address of the user
+    :param password1: The password of the user
+    :param password2: The confirmation of the password
+    :param agreed: Result of the checkbox whether the user agreed the term of the condition or not
+    :param redirect_url: The url to redirect when the registration was successful
+    :param product: The subscription plan chosen
+    """
+    MIN_EMAIL_LEN = 6
+    MIN_PASSWORD_LEN = 6
+    products = ["BASIC", "BUNDLED", "DELUXE"]
+
+    if UserDB.has(email):
+        flash("The email is already used. Please try another email.", category="Error")
+    elif len(email) < MIN_EMAIL_LEN:
+        flash("Email must be at least " + str(MIN_EMAIL_LEN) + " characters.", category="Error")
+    elif password2 is not None and password1 != password2:
+        flash("Two passwords don't match", category="Error")
+    elif len(password1) < MIN_PASSWORD_LEN:
+        flash("Password must be at least " + str(MIN_PASSWORD_LEN) + " characters.", category="Error")
+    elif agreed is not None and agreed != "on":
+        flash("You must agree to the terms and conditions", category="Error")
+    elif product is not None and product not in products:
+        flash("The product " + product + " is not valid. Please choose one of these: " + str(products), category="Error")
+    else:
+        # Add the new user account to the database
+        UserDB.add(email, password1)
+
+        # Keep the user logged in
+        user = UserDB.get(email)
+        login_user(user, remember=True)
+
+        flash("Account has been successfully created!", category="Success")
+
+        return redirect(url_for(redirect_url))  # redirect the user
 
 
 @auth_bp.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
     # Reads the input data when the user presses the submit button
     if request.method == "POST":
-        email = request.form.get("email")
-        password1 = request.form.get("password1")
-        password2 = request.form.get("password2")
 
-        if UserDB.has(email):
-            flash("The email is already used. Please try another email.", category="Error")
-        elif len(email) < MIN_EMAIL_LEN:
-            flash("Email must be at least " + str(MIN_EMAIL_LEN) + " characters.", category="Error")
-        elif password1 != password2:
-            flash("Two passwords don't match", category="Error")
-        elif len(password1) < MIN_PASSWORD_LEN:
-            flash("Password must be at least " + str(MIN_PASSWORD_LEN) + " characters.", category="Error")
-        else:
-            # Add the new user account to the database
-            UserDB.add(email, password1)
+        email = request.form.get("email13")
+        if email is None:
+            email = request.form.get("email110")
+        if email is None:
+            email = request.form.get("email115")
+        if email is None:
+            email = request.form.get("email120")
 
-            # Keep the user logged in
-            user = UserDB.get(email)
-            login_user(user, remember=True)
+        password1 = request.form.get("password12")
+        if password1 is None:
+            password1 = request.form.get("password17")
+        if password1 is None:
+            password1 = request.form.get("password110")
+        if password1 is None:
+            password1 = request.form.get("password113")
 
-            flash("Account has been successfully created!", category="Success")
+        password2 = request.form.get("repeat-password1")
+        if password2 is None:
+            password2 = request.form.get("repeat-password11")
+        if password2 is None:
+            password2 = request.form.get("repeat-password12")
+        if password2 is None:
+            password2 = request.form.get("repeat-password13")
 
-            return redirect(url_for("general.home"))  # redirect the user to the homepage
+        agreed = request.form.get("checkbox1")
+        if agreed is None:
+            agreed = request.form.get("checkbox2")
+        if agreed is None:
+            agreed = request.form.get("checkbox3")
+        if agreed is None:
+            agreed = request.form.get("checkbox4")
+
+        register(email, password1, password2, agreed, "payment.payment")
 
     return render_template("signup-page.html", user=current_user)
+
+
+@auth_bp.route("/password-recovery", methods=["GET", "POST"])
+def password_recovery():
+    if request.method == "POST":
+
+        email = request.form.get("email14")
+        if email is None:
+            email = request.form.get("email111")
+        if email is None:
+            email = request.form.get("email116")
+        if email is None:
+            email = request.form.get("email121")
+
+    return render_template("password-recovery-page.html", user=current_user)
 
 
 @auth_bp.route("/logout")
--- view/general.py
+++ view/general.py
@@ -1,11 +1,40 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import current_user, login_required
 
 general_bp = Blueprint("general", __name__)
 
 
-@general_bp.route("/")
+@general_bp.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+
+        email = request.form.get("email1")
+        if email is None:
+            email = request.form.get("email17")
+        if email is None:
+            email = request.form.get("email112")
+        if email is None:
+            email = request.form.get("email117")
+
+        password = request.form.get("password1")
+        if password is None:
+            password = request.form.get("password15")
+        if password is None:
+            password = request.form.get("password18")
+        if password is None:
+            password = request.form.get("password111")
+
+        product = request.form.get("product21")
+        if product is None:
+            product = request.form.get("product23")
+        if product is None:
+            product = request.form.get("product25")
+        if product is None:
+            product = request.form.get("product27")
+
+        from view.auth import register
+        register(email=email, password1=password, redirect_url="payment.payment", product=product)
+
     return render_template("landing-page.html", user=current_user)
 
 
@@ -18,4 +47,14 @@ def contact():
 @login_required     # Make sure the user can't access their profile page when not logged in
 def profile():
     return render_template("profile-page.html", user=current_user)
+
+
+@general_bp.route("/tutorial")
+def tutorial():
+    return render_template("video-player-page.html", user=current_user)
+
+
+@general_bp.route("/tutorial2")
+def tutorial2():
+    return render_template("video-player-page.html", user=current_user)
 
