5ad9821414a60e010303dd46b07384474cf1730d
Adam Bouthillette <adambouthillette@gmail.com>
2021-03-13 20:58:12 -0600

    Added comments

--- view/contact.py
+++ view/contact.py
@@ -7,9 +7,14 @@ contact_bp = Blueprint("contact", __name__)
 mail = Mail(contact_bp)
 
 @contact_bp.route('/contact', methods=["GET", "POST"])
+"""
+This function checks all input fields on contact-page.html and if they are correctly filled in, will use an email 
+address to send a message to a company email.
+"""
 def contact():
     if request.method == 'POST':
 
+        # Checking email input field is filled for all page format types
         email = request.form.get("email11")
         if email is None:
             email = request.form.get("email18")
@@ -18,6 +23,7 @@ def contact():
         if email is None:
             email = request.form.get("email118")
 
+        # Checking subject input field is filled for all page format types
         subject = request.form.get("subject1")
         if subject is None:
             subject = request.form.get("subject11")
@@ -26,6 +32,7 @@ def contact():
         if subject is None:
             subject = request.form.get("subject13")
 
+        # Checking message input field is filled for all page format types
         message = request.form.get("write-your-message-here1")
         if message is None:
             message = request.form.get("write-your-message-here11")
@@ -34,6 +41,7 @@ def contact():
         if message is None:
             message = request.form.get("write-your-message-here13")
 
+        # if any field has no user input, flashes screen prompting correction
         if len(email) == 0:
             flash("Please enter your email address.", category="Error")
         elif len(subject) == 0:
@@ -42,7 +50,9 @@ def contact():
             flash("Please enter a message.", category="Error")
 
         else:
-            msg = Message(sender='TheFinTechOrgTest@gmail.com', recipients=['TheFinTechOrgTest@gmail.com'])
+            # If past all prior if-statements, prepare to send message
+            msg = Message(subject.data, sender='TheFinTechOrgTest@gmail.com', recipients=['TheFinTechOrgTest@gmail.com'])
+
             msg.body = """ From: %s 
                 %s """ % (email.data, message.data)
             mail.send(msg)
@@ -51,4 +61,5 @@ def contact():
             return render_template('contact-page.html', user=current_user)
 
     else:
+        # load up contact page
         return render_template('contact-page.html', user=current_user)
