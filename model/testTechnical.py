8fe691f812bb765b35571ba931c7a7aafb6f7fa7
Bathiya <bathiya@ualberta.ca>
2021-03-23 23:41:51 -0600

    Created test script for technical

---
+++ model/testTechnical.py
@@ -0,0 +1,6 @@
+stock = Technical("aapl")
+print(stock.get_rsi())
+print(stock.get_mass_index())
+print(stock.get_pivot_fib())
+print(stock.get_macd())
+print( stock.get_simple_moving_average_range_30_10())
