dc96d2ebf48e10966597623258cd1f29e4545f46
Bathiya <bathiya@ualberta.ca>
2021-03-26 10:22:33 -0600

    started test sript

--- model/testTechnical.py
+++ model/testTechnical.py
@@ -1,3 +1,15 @@
+
+"""
+Test Script written by Bathiya Ranasinghe for the Technical Class
+Errors within the methods will be printed out to console
+
+A test complete message will be printed at the end along with the time to execute getting the data
+The script uses a range of values for certain tests and these ranges can change depending on current market conditions
+So if an error is reported on data output it is also necessary to manually check on external sources to verify if the error
+is valid or if there was a change in market conditions.
+
+"""
+
 from model.Technical import Technical
 import ray
 
