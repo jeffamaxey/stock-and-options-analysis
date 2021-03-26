fcf746de3ae5ac6631b7f49cfd7ae45d945e1d68
Bathiya <bathiya@ualberta.ca>
2021-03-26 10:10:15 -0600

    started test sript

--- model/testTechnical.py
+++ model/testTechnical.py
@@ -1,12 +1,10 @@
-import Technical as Technical
+from model.Technical import Technical
 import ray
 
 # initialize multiprocessing
 ray.init(ignore_reinit_error=True)
 
-stock = Technical.Technical("aapl")
-print(stock.get_rsi())
-print(stock.get_mass_index())
-print(stock.get_pivot_fib())
-print(stock.get_macd())
-print( stock.get_simple_moving_average_range_30_10())
+ticker = "aapl"
+tech = Technical(ticker)
+print(tech.to_string_summary())
+
