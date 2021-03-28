6181a6b2597ebea37fa3532bb4ae32fc75875042
Sahngwoo Kim (sak772) <sak772@mail.usask.ca>
2021-03-28 11:42:44 -0600

    redirecting the user in the special cases

--- view/payment.py
+++ view/payment.py
@@ -1,18 +1,23 @@
 from flask import Blueprint, render_template
 from flask_login import current_user, login_required
+from flask import request, flash, redirect, url_for
 
 payment_bp = Blueprint("payment", __name__)
 
 PRODUCTS = ("BASIC", "BUNDLED", "DELUXE")
 
 
-@payment_bp.route("/payment")
+@payment_bp.route("/payment", methods=["GET", "POST"])
 @login_required
 def payment():
+    if request.method == "POST":
+        flash("Our website is currently free!", category="Info")
+        return redirect(url_for("general.home"))    # redirect the user to the homepage
+
     return render_template("payment-processing-page.html", user=current_user)
 
 
-@payment_bp.route("/payment2")
+@payment_bp.route("/payment2", methods=["GET", "POST"])
 @login_required
 def payment2():
-    return render_template("payment-processing-page2.html", user=current_user)
+    return redirect(url_for("general.home"))    # redirect the user to the homepage
